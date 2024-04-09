78a7e99a398bce2dcf413c25881868914206f178
TAVI <octavianpatilea@gmail.com>
2024-04-09 19:00:49 +0300

    Updated the names of some variables within existing validators

--- src/main/java/org/birthleft/validator/CapacityValidator.java
+++ src/main/java/org/birthleft/validator/CapacityValidator.java
@@ -6,17 +6,17 @@ import org.birthleft.mapper.StringToIntegerMapper;
 
 public class CapacityValidator {
     public static Integer compute(String capacity, Integer line) {
-        String[] capacityParts = capacity.split(" ", 2);
-        if (capacityParts.length != 2) {
+        String[] capacityChunks = capacity.split(" ", 2);
+        if (capacityChunks.length != 2) {
             throw new CapacityValidatorException(line, "Can't be split into two parts!");
         }
-        if (capacityParts[0].equals("no") && !capacityParts[1].equals("limit")) {
+        if (capacityChunks[0].equals("no") && !capacityChunks[1].equals("limit")) {
             throw new FileInvalidSegmentException(line, "The only way to put 'no' is with 'limit'!");
         }
-        if (!(capacityParts[1].equals("part at a time") || capacityParts[1].equals("parts at a time"))) {
+        if (!(capacityChunks[1].equals("part at a time") || capacityChunks[1].equals("parts at a time"))) {
             throw new FileInvalidSegmentException(line, "If there is a limit, it must be followed by 'part at a time' or 'parts at a time'!");
         }
-        Integer result = StringToIntegerMapper.convert(capacityParts[0]);
+        Integer result = StringToIntegerMapper.convert(capacityChunks[0]);
         if (result == -1) {
             throw new FileInvalidSegmentException(line, "Number cannot be mapped!");
         }
--- src/main/java/org/birthleft/validator/CooldownTimeValidator.java
+++ src/main/java/org/birthleft/validator/CooldownTimeValidator.java
@@ -1,24 +1,23 @@
 package org.birthleft.validator;
 
 import org.birthleft.exception.CooldownTimeValidatorException;
-import org.birthleft.exception.FileInvalidSegmentException;
 
 public class CooldownTimeValidator {
     public static Integer compute(String cooldownTime, Integer line) {
-        String[] cooldownParts = cooldownTime.split(" ", 2);
+        String[] cooldownTimeChunks = cooldownTime.split(" ", 2);
         Integer result = -1;
-        if (cooldownParts.length == 1) {
-            if (!cooldownParts[0].trim().equals("none")) {
+        if (cooldownTimeChunks.length == 1) {
+            if (!cooldownTimeChunks[0].trim().equals("none")) {
                 throw new CooldownTimeValidatorException(line, "The only way to have only one word is with 'none'!");
             }
             result = 0;
         }
-        else if (cooldownParts.length == 2) {
-            if (!cooldownParts[1].trim().equals("seconds after each part")) {
+        else if (cooldownTimeChunks.length == 2) {
+            if (!cooldownTimeChunks[1].trim().equals("seconds after each part")) {
                 throw new CooldownTimeValidatorException(line, "If there is a cooldown time, it must be followed by 'seconds after each part'!");
             }
             try {
-                result = Integer.parseInt(cooldownParts[0]);
+                result = Integer.parseInt(cooldownTimeChunks[0]);
             } catch (NumberFormatException e) {
                 throw new CooldownTimeValidatorException(line, "Cooldown time must be a number!");
             }
